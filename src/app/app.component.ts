5d414e394db02d5c174fbb1a3c088b86786ce47e
TODO1\mcuellar <mcuellar@todo1.com>
2018-10-04 15:29:38 -0500

    add person information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   person = {
+     name: 'manuel',
+     age: 23
+
+
+   };
 }
